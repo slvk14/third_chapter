185d4b0956987ddb35626a35ccda311114229b82
slvk14 <slavikslavik1414@gmail.com>
2019-04-17 10:23:48 +0300

    Host name for mailer added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'tranquil-plains-56738.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
