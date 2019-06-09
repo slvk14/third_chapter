bcbb5e49dc2f95e160d12c2a974ce3c6be869c68
slvk14 <slavikslavik1414@gmail.com>
2019-06-09 22:00:06 +0300

    santa some changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,6 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       @user.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
       @user.activate
       redirect_to root_url
     else
